b733a8907529a0110c724757327cbc37f543c7b7
Nahuel <nahuel.profesor@gmail.com>
2023-01-01 21:30:28 -0300

    Trabajando en el input range

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 
 function App() {
   
+  const [cantidad, setCantidad] = useState(10000);
+  
+  function handleChange(e){
+    // console.log(Number(e.target.value));
+
+  }
+
+  
+
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <input 
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600" 
+        onChange={handleChange}
+      />
+      
     </div>
   )
 }
