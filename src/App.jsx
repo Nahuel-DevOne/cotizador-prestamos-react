eddbfdeb7c4629d7c5e7f1c62438de388c50e381
Nahuel <nahuel.profesor@gmail.com>
2023-01-03 12:52:37 -0300

    Pago mensual terminado, proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ function App() {
   const [cantidad, setCantidad] = useState(10000);
   const [meses, setMeses] = useState(6);
   const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
 
   useEffect(() => {
+    // Calcular el resultado total a pagar
     const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
     setTotal(resultadoTotalPagar);
   }, [cantidad, meses]);
+
+  useEffect(() => {
+    // Calcular el pago mensual
+    setPago(total / meses);
+  }, [total])
 
   const MIN = 0;
   const MAX = 20000;
@@ -94,7 +101,7 @@ function App() {
 
         <p className='text-xl text-gray-500 text-center font-bold'>{meses} Meses</p>
         <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(total)} Total a pagar</p>
-        <p className='text-xl text-gray-500 text-center font-bold'>Mensuales</p>
+        <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(pago)} Mensuales</p>
       </div>
     </div>
   )
